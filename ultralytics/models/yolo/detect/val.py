112f034f1484f4d8642f2bc77ddd7c3916ff7459
EngineerHumo <jiaqi.guo@robotrak.cn>
2025-11-04 01:21:23 +0800

    Add recall50 metrics and best epoch tracking

--- ultralytics/models/yolo/detect/val.py
+++ ultralytics/models/yolo/detect/val.py
@@ -104,7 +104,16 @@ class DetectionValidator(BaseValidator):
 
     def get_desc(self) -> str:
         """Return a formatted string summarizing class metrics of YOLO model."""
-        return ("%22s" + "%11s" * 6) % ("Class", "Images", "Instances", "Box(P", "R", "mAP50", "mAP50-95)")
+        return ("%22s" + "%11s" * 7) % (
+            "Class",
+            "Images",
+            "Instances",
+            "Box(P",
+            "R",
+            "R@50",
+            "mAP50",
+            "mAP50-95)",
+        )
 
     def postprocess(self, preds: torch.Tensor) -> list[dict[str, torch.Tensor]]:
         """
